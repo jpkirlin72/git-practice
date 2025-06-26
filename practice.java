6bf90bf1e04e59bd287413bcabc4b0f109438727
Jared Kirlin <jpkirlin72@gmail.com>
2025-06-25 21:28:36 -0600

    Second practice attempt

---
+++ practice.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class practice {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        int x = 5;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int y = keyboard.nextInt();
+        System.out.println("x + y = " + (x + y));
+
+    }
+
+}
